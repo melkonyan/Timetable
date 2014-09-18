1d792eadafbe0b2cc64b1642833d54aca8fb1ec5
Oleksandr Melkonyan <sasha.melkonyan@gmail.com>
2014-09-18 10:12:40 +0200

    Fix #68

--- src/com/timetable/android/activities/EventDayViewActivity.java
+++ src/com/timetable/android/activities/EventDayViewActivity.java
@@ -113,17 +113,20 @@ public class EventDayViewActivity extends Activity {
 			
 		};
 		
+		initEventPager();
+	}
+	
+	/*
+	 * Create event pager. 
+	 * If intent contains extra date, set current displayed date.
+	 */
+	private void initEventPager() {
 		Date startDate = getExtraDate();
 		if (startDate != null) {
 			TimetableLogger.log("Start EventDayViewActivity with extra date: " + startDate.toString());
 			setEventPager(new EventPager(this,startDate));
 			//eventPager.goToDate(startDate);
 		}
-		Calendar cal = Calendar.getInstance();
-		cal.setTime(getEventPager().getDisplayedDate());
-		datePickerDialog = DatePickerDialog.newInstance(mOnDateSetListener, 
-				cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
-
 	}
 
 
@@ -138,6 +141,14 @@ public class EventDayViewActivity extends Activity {
 	public void onDestroy() {
 		super.onDestroy();
 		TimetableLogger.sendReport();
+	}
+	
+	@Override 
+	public void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		TimetableLogger.log("EventDayViewAvtivity: new intent received!");
+		setIntent(intent);
+		initEventPager();
 	}
 	
 	@Override
