30b9ae336c1fb7d44ab3f190b37531f6d5fbc4ae
Oleksandr Melkonyan <sasha.melkonyan@gmail.com>
2014-09-18 18:39:31 +0200

    Remove unused imports

--- src/com/timetable/android/activities/EventAddActivity.java
+++ src/com/timetable/android/activities/EventAddActivity.java
@@ -4,7 +4,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.TimeZone;
 
 import org.holoeverywhere.app.Activity;
 import org.holoeverywhere.widget.AdapterView;
