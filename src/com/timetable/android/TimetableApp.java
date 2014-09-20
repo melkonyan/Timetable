95218f4010d877708d19f1aa90fe0d3e53a13546
Oleksandr Melkonyan <sasha.melkonyan@gmail.com>
2014-09-20 18:04:02 +0200

    Turn on acra

--- src/com/timetable/android/TimetableApp.java
+++ src/com/timetable/android/TimetableApp.java
@@ -28,13 +28,13 @@ public class TimetableApp  extends Application {
 		//enable debugging
 		TimetableLogger.debugging = true;
 			
-		TimetableLogger.sendReport = false;
+		TimetableLogger.sendReport = true;
 		
 		//send broadcast, that application is started.
 		sendBroadcast(new Intent(BroadcastActions.ACTION_APP_STARTED));
 			
 		//initialize acra
-		//ACRA.init(this);
+		ACRA.init(this);
 		TimetableLogger.log("TimetableApp.onCreate: Application is created. ACRA is initialized");
 	}
 
