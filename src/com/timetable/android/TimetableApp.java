9d0f69a33b771b148334bad1c182a0b41baeadb1
Oleksandr Melkonyan <sasha.melkonyan@gmail.com>
2014-10-19 12:25:28 +0200

    enable reportinh

--- src/com/timetable/android/TimetableApp.java
+++ src/com/timetable/android/TimetableApp.java
@@ -28,7 +28,7 @@ public class TimetableApp  extends Application {
 		//enable debugging
 		TimetableLogger.debugging = true;
 			
-		TimetableLogger.sendReport = false;
+		TimetableLogger.sendReport = true;
 		
 		//send broadcast, that application is started.
 		sendBroadcast(new Intent(BroadcastActions.ACTION_APP_STARTED));
